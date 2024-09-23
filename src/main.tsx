37bc25bf85643dd70d83ea6fb105084922620043
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-09-23 15:08:21 -0300

    feat: remove unused files + add favicon + create home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Home } from './pages/home';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Home />
+  </StrictMode>
+);
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+    <div>
+      <h1>Ola mundo</h1>
+    </div>
+  );
+};
